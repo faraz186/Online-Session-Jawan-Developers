ba3b84ebc3cde7c9069e7174b20c9b671e5516b3
Mohammad Faraz <69837700+faraz186@users.noreply.github.com>
2023-01-20 00:49:25 +0500

    Add files via upload

---
+++ Online Session of Jawan Developers/app.js
@@ -0,0 +1,322 @@
+// alert("Hello WORLD");
+
+// alert is a builtin function
+
+// var naam = "XYZ";
+
+// naam = "ABC";
+
+// console.log(naam);
+
+
+
+// alert(name);
+// document.write(name);
+// console.log(name);
+
+// ----------------------------
+
+// var number = 50;
+
+// //     50    += 25
+// // // number += 25;
+
+// number = number + 25;
+
+// console.log(number)
+
+
+
+// ******************************
+
+// var user = +prompt("Enter a number");
+
+// user = user + 25;
+
+// console.log(user)
+
+
+// ******************************
+
+// illegal variables
+
+// var user-NAME
+// var 123user
+// var user.name
+// var user Name
+
+
+// legal variables
+
+// var userName
+
+// var user_name
+
+// var name123
+
+// ************************************
+
+var num1 = (2*5) + 5 * 9 - 10;
+
+// BODMAS
+
+// BRACKET
+// DIVIDE
+// MUILTIPLY
+// ADDITION
+// SUBTRACTION
+
+// console.log(num1)
+// **********************************
+
+// var num = 1;
+
+// console.log(++num);
+
+// console.log(num++);
+
+// console.log(num)
+
+// var num = 2;
+
+// console.log(num--);
+
+// console.log(num)
+
+// console.log(--num);
+
+// ******************************
+
+// var num = 10;
+
+// num = num++;
+//       9  -  10 = -1
+// num = --num - ++num;
+
+// console.log(num);
+
+
+// ***********************************
+
+
+// var firstName = "Mohammad";
+
+// var lastname = "Faraz";
+
+// var fullName = firstName + " " + lastname;
+
+// console.log(fullName);
+
+
+
+// var firstName = "Mohammad";
+
+// var lastname = "Faraz";
+
+// var fullName = firstName.concat(" " + lastname);
+
+// console.log(fullName);
+
+// var city = prompt("Enter city name");
+
+// if(city == "hyderabad")
+// {
+//     console.log("Cleanest City")
+// }
+
+// else{
+//     console.log("Not a Cleanest City")
+// }
+
+// var rollNo = +prompt("Enter roll no");
+
+// if(rollNo !== 425988)
+// {
+//     console.log("Enter Please")
+// }
+// else{
+//     console.log("Get out")
+// }
+
+
+// var weight = 300;
+
+// var age = 22;
+
+// if(weight <=300 || age<21)
+// {
+//     alert("You are eligible")
+// }
+
+// else if(weight >300 && age<30)
+// {
+//     alert("You are not eligible")
+// }
+
+// *****************************
+
+// var city = "karachi";
+
+// var city2 = "islamabad";
+
+// var city3 = "Peshawar";
+                
+// var arr = ["Karachi","islamabad",56,true,100,false,"hyderabad"];
+
+// var arr = ["Karachi","isla
+
+// var arr = ["karachi",12,"kangaroo",false,"Fox"];
+
+var arr = [12,false,"london",89,"karachi","islamabad"];
+
+// console.log(arr.slice(0,3));
+
+// arr.splice(2,2,"America");
+
+// arr.slice(1,4)
+
+// arr.slice(1);
+// alert(arr)
+// arr.pop();
+// arr.pop();
+// arr.push("turkey");
+// arr.push(false);
+// arr.shift();
+// arr.shift();
+// arr.shift();
+// arr.unshift("karachi");
+// arr.unshift(18);
+
+
+// console.log(arr)
+
+
+// console.log(arr[6]);
+
+
+// var arr = ["karachi","islamabad",54,false];
+
+// for(var i = 0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+
+// var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+// var fullNames = [];
+
+// for(var i =0;i<firstNames.length;i++)
+// {
+//     for(var j = 0 ; j < lastNames.length;j++)
+//     {
+//         fullNames.push(firstNames[i]  + lastNames[j])
+//     }
+// }
+
+// console.log(fullNames)
+
+//  for (var i = 0; i < firstNames.length; i++) 
+//  {
+//  for (var j = 0; j < lastNames.length; j++) 
+//  {
+//  fullNames.push(firstNames[i] + lastNames[j]);
+//  }
+//  }
+
+// var num = 12.94;
+
+// var res = Math.floor(num);
+
+// var random = Math.random()
+
+// console.log(random.toFixed(4));
+
+// console.log(res);
+
+var bigDecimal = Math.random() * 6;
+
+var luddo = bigDecimal + 1;
+
+var res = Math.floor(luddo);
+
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
